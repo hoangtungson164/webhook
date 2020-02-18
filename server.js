22f34124a2bbbb37d800db2cbdcd542477a09cfb
Hoang Tung Son <hoangtungson164@gmail.com>
2020-02-18 16:16:51 +0700

    add file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
   console.log('sdfasdlkfjsldkfjasdf');
-  response.end('Hello Internet! Version 2 !!!!!!!!adfd');
+  response.end('Hello Internet! Version 002');
 };
 var www = http.createServer(handleRequest);
 console.log('Listening on http://localhost:' + port);
